547c5de6a5f21a3cd8ead87c58ed6f48e7519952
iversonLv <iverson_3_3_3@126.com>
2023-03-20 16:28:22 +0800

    Use NavLink in order that no issue for hash Router

--- src/components/common/HeaderMenu/Menu.js
+++ src/components/common/HeaderMenu/Menu.js
@@ -1,9 +1,9 @@
-import { Box, Divider, IconButton, Link, List, ListItem, ListItemButton, ListItemText, Typography } from "@mui/material";
+import { Box, Divider, IconButton, List, ListItem, ListItemButton, ListItemText, Typography } from "@mui/material";
 import { APP_HEADING, HOME_URL, loginedNavItem, nonLoginedNavItem, SIGN_IN_UP_URL } from "../../../constants";
 
 // Mui icons
 import CloseOutlinedIcon from '@mui/icons-material/CloseOutlined';
-import { useLocation, useNavigate } from "react-router-dom";
+import { useLocation, useNavigate, NavLink } from "react-router-dom";
 import useAuth from "../../../firebase/auth";
 
 const Menu = ({toggleDrawer}) => {
@@ -43,14 +43,14 @@ const Menu = ({toggleDrawer}) => {
     <Divider />
     <List>
       <ListItem disablePadding>
-        <ListItemButton component={Link} to={HOME_URL} selected={HOME_URL === location.pathname}>
+        <ListItemButton component={NavLink} to={HOME_URL} selected={HOME_URL === location.pathname}>
           <ListItemText primary='Home' />
         </ListItemButton>
       </ListItem>
       
       {!!authUser && !isLoading && loginedNavItem.map(item => (
         <ListItem key={item.label} disablePadding>
-          <ListItemButton component={Link} to={item.route} selected={item.route === location.pathname}>
+          <ListItemButton component={NavLink} to={item.route} selected={item.route === location.pathname}>
             <ListItemText primary={item.label} />
           </ListItemButton>
         </ListItem>
@@ -62,7 +62,7 @@ const Menu = ({toggleDrawer}) => {
       {/* If not logined, show login and register button */}
       {!authUser && !isLoading && nonLoginedNavItem.map((item) => (
         <ListItem key={item.label} disablePadding>
-          <ListItemButton component={Link} to={item.route} selected={item.route === location.pathname}>
+          <ListItemButton component={NavLink} to={item.route} selected={item.route === location.pathname}>
             <ListItemText primary={item.label} />
           </ListItemButton>
         </ListItem>
